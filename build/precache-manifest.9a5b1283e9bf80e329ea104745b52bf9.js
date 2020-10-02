dfc95d75face72959e9b365e931bd4aa97c762ae
felixfrz <49365034+felixfrz@users.noreply.github.com>
2020-10-02 21:14:33 +0100

    Add files via upload

---
+++ build/precache-manifest.9a5b1283e9bf80e329ea104745b52bf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2995899db2e1fd67b0e0ae2b6dbb8ab6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee3e2bde6cf9fb7d6219",
+    "url": "/static/css/main.9b4af9e3.chunk.css"
+  },
+  {
+    "revision": "e6eb8e2ed3eaff22fd40",
+    "url": "/static/js/2.8e7f537c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8e7f537c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee3e2bde6cf9fb7d6219",
+    "url": "/static/js/main.1c98b973.chunk.js"
+  },
+  {
+    "revision": "75cf2bf6a8420bcb5ce6",
+    "url": "/static/js/runtime-main.e0b212d8.js"
+  }
+]);
